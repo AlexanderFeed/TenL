548b31bbfc50996cd93b5978469f570188a80f10
Дмитрий Писаренко <dimapisarenko28@gmail.com>
2022-06-25 23:55:39 +0300

    Trash cleaning

--- FLib/Console.fs
+++
@@ -1,6 +0,0 @@
-﻿module Console
-
-type InsideBase(name: string, manufacture: string, release_date: int32, price: double, cpu:string, ram: string, generation: string, vram: string,image_url: string) =
-    inherit FBase(name, manufacture, release_date, price, cpu, ram, generation, vram,image_url)
-    member this.Generation = generation
-    member this.VRAM = vram
--- FLib/PortaConsole.fs
+++
@@ -1,6 +0,0 @@
-﻿module PortaConsole
-
-type InsideBase(name: string, manufacture: string, release_date: int32, price: double, cpu:string, ram: string, generation: string, vram: string, power_supply: string, screen: int32,image_url: string) =
-    inherit FBase(name, manufacture, release_date, price, cpu, ram, generation, vram, power_supply, screen,image_url)
-    member this.Screen = screen
-    member this.Power_supply = power_supply
